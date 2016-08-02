1ee93185fb5a419af82d3e037cdbb75d44158aa5
Nir Shemesh <nvshemesh@gmail.com>
2016-08-02 15:04:45 +0300

    Implemented Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+# Required for Heroku
+# Note to set this as the actual host name
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
