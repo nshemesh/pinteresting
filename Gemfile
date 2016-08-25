aff82ca4bd78ae13e4ae535275bfb54e91ab301f
Nir Shemesh <nvshemesh@gmail.com>
2016-08-25 12:54:30 +0300

    Add Amazon S3 for Paperclip uploads to Heroku

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise', '~> 4.1', '>= 4.1.1'
 gem 'paperclip', '~> 4.2'
+gem 'aws-sdk', '< 2.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,15 @@ Rails.application.configure do
 # Note to set this as the actual host name
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+# Sets Paperclip to upload images to Amazon S3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
